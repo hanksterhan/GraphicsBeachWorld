84400fa07d4abfa66aca208357fca5aa1f93b986
hanksterhan <henryhan62@gmail.com>
2019-05-14 23:52:26 +0200

    code after a practical, implemented basic ray tracing and clipped quadrics

---
+++ js/Scene.js
@@ -0,0 +1,91 @@
+"use strict";
+const Scene = function(gl) {
+  this.vsQuad = new Shader(gl, gl.VERTEX_SHADER, "quad_vs.essl");
+  this.fsTrace = new Shader(gl, gl.FRAGMENT_SHADER, "trace_fs.essl");
+  this.traceProgram = new TexturedProgram(gl, this.vsQuad, this.fsTrace);
+
+  this.texturedQuadGeometry = new TexturedQuadGeometry(gl);  
+
+  this.timeAtFirstFrame = new Date().getTime();
+  this.timeAtLastFrame = this.timeAtFirstFrame;
+
+  this.traceMaterial = new Material(gl, this.traceProgram);
+  this.traceMaterial.envmap.set(new TextureCube(gl, [
+    "media/posx.jpg",
+    "media/negx.jpg",
+    "media/posy.jpg",
+    "media/negy.jpg",
+    "media/posz.jpg",
+    "media/negz.jpg",]
+  	));
+
+  this.traceMesh = new Mesh(this.texturedQuadGeometry, this.traceMaterial);
+
+  this.gameObjects = [];
+  this.gameObjects.push(new GameObject(this.traceMesh));
+
+  this.camera = new PerspectiveCamera();
+  this.camera.position.set({x:0, y:3, z:15});
+
+  // incrementally rendered object to demonstrate combining the incremental rendering and ray casting
+  this.vsTrafo = new Shader(gl, gl.VERTEX_SHADER, "trafo_vs.essl");
+  this.fsTextured = new Shader(gl, gl.FRAGMENT_SHADER, "textured_fs.essl");
+  this.texturedProgram = new TexturedProgram(gl, this.vsTrafo, this.fsTextured);
+  this.slowpokeMaterials = [
+    new Material(gl, this.texturedProgram),
+    new Material(gl, this.texturedProgram),
+    ];
+  this.slowpokeMaterials[0].colorTexture.set(
+    new Texture2D(gl, 'media/slowpoke/YadonDh.png'));
+  this.slowpokeMaterials[1].colorTexture.set(
+    new Texture2D(gl, 'media/slowpoke/YadonEyeDh.png'));  
+
+  this.slowpokeMesh = new MultiMesh(
+    gl,
+    'media/slowpoke/Slowpoke.json',
+    this.slowpokeMaterials
+    );
+  // this.gameObjects.push(new GameObject(this.slowpokeMesh));
+
+  Uniforms.scene.surfaces.at(0).set(
+    1.0, 0.0, 0.0, 0.0,
+    0.0, 1.0, 0.0, 0.0,
+    0.0, 0.0, 1.0, 0.0, 
+    0.0, 0.0, 0.0,-9.0 
+  );
+
+  Uniforms.scene.clippers.at(0).set(
+    1.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 
+    0.0, 0.0, 0.0,-4.0 
+  );
+
+  gl.enable(gl.DEPTH_TEST);
+};
+
+Scene.prototype.update = function(gl, keysPressed) {
+  //jshint bitwise:false
+  //jshint unused:false
+  const timeAtThisFrame = new Date().getTime();
+  const dt = (timeAtThisFrame - this.timeAtLastFrame) / 1000.0;
+  const t = (timeAtThisFrame - this.timeAtFirstFrame) / 1000.0; 
+  this.timeAtLastFrame = timeAtThisFrame;
+
+  // clear the screen
+  gl.clearColor(0.3, 0.0, 0.3, 1.0);
+  gl.clearDepth(1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  this.camera.move(dt, keysPressed);
+
+  Uniforms.camera.position.set(this.camera.position);
+  Uniforms.camera.viewProjMatrix.set(this.camera.viewProjMatrix);  
+  Uniforms.trafo.rayDirMatrix.set(this.camera.rayDirMatrix);
+
+  for(let i=0; i<this.gameObjects.length; i++){
+    this.gameObjects[i].draw(this.camera);
+  }
+};
+
+
